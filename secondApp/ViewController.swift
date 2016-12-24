ab0ced478f42bd99cc4a32270590d2bc055c98f5
Bill Gao <billgao@BilldeiMac.local>
2016-12-24 16:48:16 -0600

    update scroll view

--- secondApp/ViewController.swift
+++ secondApp/ViewController.swift
@@ -10,23 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+ 
+    
     @IBOutlet weak var scroll: UIScrollView!
     var images = [UIImageView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
         // Do any additional setup after loading the view, typically from a nib.
+        let scrollWidth = scroll.frame.size.width
         for x in 0...2{
             let image = UIImage(named:"icon\(x).png")
             let imageView = UIImageView(image: image)
             images.append(imageView)
             var newX: CGFloat  = 0.0
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            contentWidth += scrollWidth * CGFloat(x)
             scroll.addSubview(imageView)
-            imageView.frame = CGRect(x: newX-75, y: view.frame.size.height/2-75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX-75, y: scroll.frame.size.height/2-75, width: 150, height: 150)
         }
-        print("Count: \(images.count)")
-        
+        scroll.clipsToBounds = false
+        scroll.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
     override func didReceiveMemoryWarning() {
